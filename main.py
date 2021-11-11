5b2e0ca83a0a920f1f0b606ab4880b3ab13bca61
gusta-glitch <gusta.justino.freitas@gmail.com>
2021-11-11 12:44:10 -0300

    CLI args, debug and change pipenv to virtualenv

--- main.py
+++ main.py
@@ -12,19 +12,29 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.select import Select
 from selenium.webdriver.support.ui import WebDriverWait
 
+from _info import __version__
+
 # Set the arguments
 
 max_amount = 100
 
-parser = argparse.ArgumentParser(description="Fill the tasklist", formatter_class=argparse.MetavarTypeHelpFormatter)
+parser = argparse.ArgumentParser(usage="%(prog)s [opitions]" ,description="Script to autofill tasklist o CMSP, write in Python", epilog="Developed by: @gustavofreitas", formatter_class=argparse.MetavarTypeHelpFormatter)
 
-parser.add_argument("--ra", "-u", type=str, help="RA of the student, with digit")
-parser.add_argument("--uf", "-c", type=str, help="UF of the student", default="sp")
-parser.add_argument("--password", "-p", type=str, help="Password of the student")
-parser.add_argument("--team", "-t", type=str, help="Team of the student")
-parser.add_argument("--amount", "-a", type=str, help="Amount of tasks to fill", default="10")
+parser.add_argument("-V", "--version", help="show version and exit", action="version", version=f"%(prog)s {__version__}")
 
-parser.add_argument("--list", "-l", type=str, help="List of user to fill tasks, files superteds: .json")
+parser_login = parser.add_argument_group("Login")
+
+parser_login.add_argument("-u", "--ra", type=str, help="RA of the student, with digit")
+parser_login.add_argument("-c", "--uf", type=str, help="UF of the student", default="sp")
+parser_login.add_argument("-p", "--password", type=str, help="password of the student")
+parser_login.add_argument("-t", "--team", type=str, help="team of the student")
+parser_login.add_argument("-a", "--amount", type=str, help="amount of tasks to fill", default="10")
+
+parser_login.add_argument("--list", "-l", type=str, help="list of user to fill tasks")
+
+parser_debug = parser.add_argument_group("Debug")
+
+parser_debug.add_argument("-v", "--verbose", help="verbose mode", action="store_true", default=False)
 
 args = parser.parse_args()
 
@@ -48,8 +58,21 @@ if args.ra and args.password and args.team:
 			if file.name.endswith(".json"):
 				arg_list = json.load(file)["users"]
 else:
-	print("No args")
+	print("No arguments")
 	exit()
+
+def debug(type, message):
+	if args.verbose:
+		if type == "error":
+			print(f"\033[91m[ERROR]\033[0m {message}")
+		elif type == "info":
+			print(f"\033[92m[INFO]\033[0m {message}")
+		elif type == "warning":
+			print(f"\033[93m[WARNING]\033[0m {message}")
+		elif type == "success":
+			print(f"\033[94m[SUCCESS]\033[0m {message}")
+		else:
+			print(f"\033[95m[DEBUG]\033[0m {message}")
 
 # Set up the driver
 
@@ -60,6 +83,8 @@ driver.get("https://cmspweb.ip.tv/") # Open the "CMSP" website
 # Set functions
 
 def login(ra, uf, password):
+	debug("info", f"Logging in with RA:{ra} and UF:{uf}")
+
 	driver.find_element(By.ID, "access-student").click() # Click on the "Student" button
 
 	driver.find_element(By.ID, "ra-student").send_keys(ra[slice(0, -1)]) # Enter the student's RA
@@ -72,10 +97,12 @@ def login(ra, uf, password):
 
 	driver.find_element(By.ID, "btn-login-student").click() # Click on the "Login" button
 
-	print(f"Logged in as {ra[slice(0, -1)]}-{ra[slice(-1, -2, -1)]} in {uf}")
+	debug("success", f"Logged in with RA:{ra} and UF:{uf}")
 
 	return 0
 def navigate(team):
+	debug("info", f"Navigating to team:{team}")
+
 	driver.implicitly_wait(1000 * 1) # Wait 1 second
 	WebDriverWait(driver, 900).until(lambda driver: driver.find_element(By.ID, "chng")).click() # Click on the "Groups" button
 
@@ -85,11 +112,11 @@ def navigate(team):
 	driver.implicitly_wait(1000 * 1) # Wait 1 second
 	WebDriverWait(driver, 900).until(lambda driver: driver.find_element(By.ID, "channelTaskList")).click() # Click on the "TaskList" button
 
-	print(f"Navigated to {team}")
+	debug("success", f"Navigated to team:{team}")
 
 	return driver.find_element(By.TAG_NAME, "iframe").get_attribute("src") # Get the "TaskList" iframe's src
 def fill(amount):
-	time.sleep(1) # Wait 1 second
+	_amount = int(amount)
 
 	tasks = driver.execute_script("return document.querySelectorAll(\"#root > div > div.homeuser > main > div.scrollable.homeuser__body--my-tasks > div > table > tbody > tr.MuiTableRow-root.row\")") # Get the tasks
 	set_amount = 0 # Set the amount of tasks to fill
@@ -98,15 +125,15 @@ def fill(amount):
 	if tasks: # If there are tasks
 		if amount == "all": # If the amount is "all"
 			set_amount = len(tasks)
-		elif int(amount) > 0 and int(amount) <= len(tasks): # If the amount is between 1 and the amount of tasks
-			set_amount = int(amount)
-		elif amount > len(tasks): # If the amount is greater than the amount of tasks
+		elif _amount > 0 and _amount <= len(tasks): # If the amount is between 1 and the amount of tasks
+			set_amount = _amount
+		elif _amount > len(tasks): # If the amount is greater than the amount of tasks
 			set_amount = len(tasks)
 		else: # If the amount is invalid
-			print("The amount of tasks is invalid")
+			debug("error", f"Invalid amount of tasks to fill: {amount}")
 			driver.close() # Close the driver
 
-		print(f"Filling {amount} tasks of {tasks}")
+		debug("info" ,f"Filling {amount} tasks of {tasks}")
 
 		for filled in range(0, set_amount):
 			for task in tasks:
@@ -125,10 +152,10 @@ def fill(amount):
 			print(f"{filled} tasks are filled")
 			conclued += 1 # Increase the amount of tasks completed
 	else:
-		print("No tasks to fill")
+		debug("error", f"No tasks to fill")
 		driver.close() # Close the driver
 
-	print(f"{conclued} tasks are conclued")
+	debug("success", f"{conclued} tasks are completed")
 
 # Run the script
 
